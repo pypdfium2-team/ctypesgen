1f798996f07fca5b417c931e216b418b59cf7848
geisserml <geisserml@gmail.com>
2024-01-21 17:29:38 +0100

    continue

--- ctypesgen/includes.py
+++ ctypesgen/includes.py
@@ -5,10 +5,10 @@ from pathlib import Path
 
 INCLUDE_RE = re.compile(rb'^\s*\#include\s+(["<][^">]+[">])', re.MULTILINE)
 
-def gather_indirect_includes(filepaths, anchor):
+def gather_includes(filepaths, anchor):
     includes = {}
     for fp in filepaths:
-        key = Path(fp).relative_to(anchor)
+        key = str(Path(fp).relative_to(anchor))
         includes[key] = []
         with open(fp, "rb") as fh:
             mm = mmap.mmap(fh.fileno(), 0, access=mmap.ACCESS_READ)
@@ -19,10 +19,23 @@ def gather_indirect_includes(filepaths, anchor):
 def filter_relative_includes(includes):
     includes = includes.copy()
     for key, file_includes in includes.items():
-        includes[key] = [m.group(1) for i in file_includes if (m := re.match(rb'"(.+)"', i))]
+        includes[key] = [m.group(1).decode() for i in file_includes if (m := re.match(rb'"(.+)"', i))]
     return includes
 
 
-def resolve_header_linkage(includes):
-    # TODO start with files that have empty lists, then pop these from all others, and recurse until we have resolved everything
-    pass
+def resolve_header_linkage(orig_includes):
+    order = []
+    includes = orig_includes.copy()
+    while True:
+        satisfied = [p for p, deps in includes.items() if not deps]
+        if not satisfied:
+            if includes:
+                raise RuntimeError(f"Some dependencies could not be satisfied: {includes}")
+            break
+        for s in satisfied:
+            order.append(s)
+            includes.pop(s)
+        for key, deps in includes.items():
+            includes[key] = [d for d in deps if d not in satisfied]
+    assert len(order) == len(orig_includes)
+    return order
--- test_includes.py
+++ test_includes.py
@@ -5,6 +5,9 @@ import rich
 headers_dir = Path("~/projects/pypdfium2/data/bindings/headers/").expanduser()
 filepaths = list(headers_dir.glob("*.h"))
 
-all_includes = gather_indirect_includes(filepaths, headers_dir)
+all_includes = gather_includes(filepaths, headers_dir)
+rich.print(all_includes)
 rel_includes = filter_relative_includes(all_includes)
 rich.print(rel_includes)
+order = resolve_header_linkage(rel_includes)
+rich.print(order)
