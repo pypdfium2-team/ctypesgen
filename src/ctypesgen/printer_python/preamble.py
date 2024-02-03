c08b0cb3ec07e5b9d815d6c20235d8e1c72a0208
geisserml <geisserml@gmail.com>
2024-02-03 22:40:38 +0100

    preamble/ptrdiff: order int types by likeliness
    We are most likely on a platform that uses 64-bit pointers.
    
    Expectably, 16-bit pointers are not a realistic target for ctypesgen,
    but retain it anyways to be C standard correct.
    
    Note, the point of this template is to achieve universal bindings. If
    bindings were target platform specific, the ptrdiff_t constant pulled in
    from stddef.h would be fine.

--- src/ctypesgen/printer_python/preamble.py
+++ src/ctypesgen/printer_python/preamble.py
@@ -2,10 +2,10 @@ import ctypes
 from ctypes import *  # noqa: F401, F403
 
 def _get_ptrdiff_t():
-    int_types = [ctypes.c_int16, ctypes.c_int32]
+    int_types = [ctypes.c_int32, ctypes.c_int16]
     # Some builds of ctypes do not provide c_int64. Assumably, this means the platform doesn't have 64-bit pointers.
     if hasattr(ctypes, "c_int64"):
-        int_types.append(ctypes.c_int64)
+        int_types.insert(0, ctypes.c_int64)
     return next((t for t in int_types if sizeof(t) == sizeof(c_size_t)), None)
 
 c_ptrdiff_t = _get_ptrdiff_t()
