1cf4cdb0e704654fecd4fa6b6f171c0f9bb47bdb
geisserml <geisserml@gmail.com>
2025-04-28 22:52:04 +0200

    Use shlex.split() for line splitting.
    This amends the previous commit.
    Also CC https://github.com/ctypesgen/ctypesgen/issues/223

--- src/ctypesgen/__main__.py
+++ src/ctypesgen/__main__.py
@@ -225,7 +225,7 @@ def input_dir_t(p):
 
 class LocalArgumentParser (argparse.ArgumentParser):
     def convert_arg_line_to_args(self, arg_line):
-        return arg_line.split()
+        return shlex.split(arg_line)
 
 
 def get_parser():
