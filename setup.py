c7d382c012b874fbad915d357dbfaa14521150ec
geisserml <geisserml@gmail.com>
2025-05-31 01:17:06 +0200

    setup: address deprecation warnings around license
    closes https://github.com/pypdfium2-team/ctypesgen/issues/25

---
+++ setup.py
@@ -0,0 +1,8 @@
+# Please see https://github.com/osvenskan/posix_ipc/issues/68 for why we are doing this.
+# We may switch to an SPDX expression in pyproject.toml once the transition has settled.
+
+import setuptools
+
+setuptools.setup(
+    license = "BSD-2-Clause",
+)
