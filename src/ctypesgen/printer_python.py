9c575e36546e8e880930badc0653e44cf1b409c5
geisserml <geisserml@gmail.com>
2025-06-17 03:58:01 +0200

    Slightly modernize unnamed fields handling
    Python doesn't have classical do-while loops, but an assignment
    expression can nicely simplify this clause. Requires Python >= 3.8.
    
    Also, use enumerate() and remove an unnecessary listcomp layer on set
    construction.

--- src/ctypesgen/printer_python.py
+++ src/ctypesgen/printer_python.py
@@ -248,21 +248,19 @@ _register_library(
         
         # handle unnamed fields
         unnamed_fields = []
-        names = set([x[0] for x in struct.members])
+        names = set(x[0] for x in struct.members)
         n = 1
-        for mi in range(len(struct.members)):
-            mem = list(struct.members[mi])
-            if mem[0] is None:
-                while True:
-                    name = f"unnamed_{n}"
-                    n += 1
-                    if name not in names:
-                        break
-                mem[0] = name
-                names.add(name)
-                if type(mem[1]) is CtypesStruct:
-                    unnamed_fields.append(name)
-                struct.members[mi] = mem
+        for mi, mem in enumerate(struct.members):
+            if mem[0] is not None:
+                continue
+            while (name := f"unnamed_{n}") in names:
+                n += 1
+            mem = list(mem)
+            mem[0] = name
+            names.add(name)
+            if type(mem[1]) is CtypesStruct:
+                unnamed_fields.append(name)
+            struct.members[mi] = mem
         
         return aligned, tuple(unnamed_fields)
 
