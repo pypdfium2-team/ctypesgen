976b57138f6088186c58e4d71024ce4b76b46a84
geisserml <geisserml@gmail.com>
2025-07-04 15:47:44 +0200

    library loader: pass through system dll names
    quick & dirty

--- src/ctypesgen/libraryloader.py
+++ src/ctypesgen/libraryloader.py
@@ -1,6 +1,7 @@
 import sys
 import ctypes
 import ctypes.util
+import os.path
 import pathlib
 
 if sys.platform.startswith(("win32", "cygwin", "msys")):
@@ -10,27 +11,29 @@ elif sys.platform.startswith(("darwin", "ios")):
 else:  # assume unix pattern or plain name
     _LIB_PREFIX, _LIB_SUFFIX = "lib", "so"
 
-def _find_library(name, libpaths, search_sys):
+def _find_library(name, dllclass, libpaths, search_sys):
     
-    # XXX For hardcoded system pdfium, this will fail if it's not a full path (e.g. on Linux). The pain points are path joining and the .is_file() check.
-    
-    for lpath in libpaths:
-        lpath = pathlib.Path(lpath)
-        if not lpath.is_absolute():
+    for lpath_str in libpaths:
+        lpath = pathlib.Path(lpath_str)
+        have_parent = bool(os.path.dirname(lpath_str))
+        if (not lpath.is_absolute()) and have_parent:
             lpath = (pathlib.Path(__file__).parent / lpath).resolve(strict=False)
-        lpath = lpath.parent / lpath.name.format(prefix=_LIB_PREFIX, name=name, suffix=_LIB_SUFFIX)
-        if lpath.is_file():
-            return lpath
+        if have_parent:
+            lpath = lpath.parent / lpath.name.format(prefix=_LIB_PREFIX, name=name, suffix=_LIB_SUFFIX)
+        try:
+            return dllclass(lpath), lpath
+        except OSError:
+            pass
     
     lpath = ctypes.util.find_library(name) if search_sys else None
     if not lpath:
-        raise ImportError(f"Could not find library '{name}' (libpaths={libpaths}, search_sys={search_sys})")
+        raise ImportError(f"Could not find library {name!r} (libpaths={libpaths}, search_sys={search_sys})")
     
-    return lpath
+    return dllclass(lpath), lpath
 
 _libs_info, _libs = {}, {}
 
 def _register_library(name, dllclass, **kwargs):
-    libpath = _find_library(name, **kwargs)
-    _libs_info[name] = {**kwargs, "path": libpath}
-    _libs[name] = dllclass(libpath)
+    dll, lpath = _find_library(name, dllclass, **kwargs)
+    _libs[name] = dll
+    _libs_info[name] = {**kwargs, "path": lpath}
